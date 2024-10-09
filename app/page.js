7b032ce77b77591fb811c3d82e7cdc1617a79475
FreakQnZ <anuraag.srivatsa123@gmail.com>
2024-10-09 11:32:58 +0530

    Landing Page with aceternity

--- app/page.js
+++ app/page.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import Image from 'next/image'
+import { TextGenerateEffect } from './components/ui/text-generate-effect'
+
+const words = "Add bills and split with ease, anywhere"
 
 const Home = () => {
   return (
-    <div></div>
+    <div className="hero bg-sky-100 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse justify-between w-full">
+                <Image
+                    src="/hero_bg.svg"
+                    width={700}
+                    height={700}
+                    className="max-w-sm p-5 " />
+                <div>
+                    {/* <h1 className=" text-7xl font-bold">Add bills and split with <br/> ease, anywhere!</h1> */}
+                    <TextGenerateEffect duration={2} filter={false} words={words} />
+                    <p className="py-6 text-xl">
+                    Securely split bills among friends, ensuring fair
+                     payments for everyone.
+                    </p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
   )
 }
 
