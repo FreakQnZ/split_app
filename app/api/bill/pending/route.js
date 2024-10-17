53560953b088221a11305bf87b06b2ad6b8e0407
FreakQnZ <anuraag.srivatsa123@gmail.com>
2024-10-17 23:09:44 +0530

    unsettled bills

---
+++ app/api/bill/pending/route.js
@@ -0,0 +1,28 @@
+import pool from "@/app/utils/connectDB";
+import { NextResponse } from "next/server";
+
+export async function GET(req) {
+    const { searchParams } = new URL(req.url);
+    const username = searchParams.get("user");
+
+    try {
+      const query1 = `SELECT user_id FROM users WHERE username = $1`;
+      const currentUserQuery = await pool.query(query1, [username]);
+      if (currentUserQuery.rowCount === 0) {
+        return NextResponse.json({
+          success: false,
+          error: "Current user not found.",
+        });
+      }
+
+      const query2 = `select bp.id as id, u.username as From, bp.amount_owed as amount, b.bill_name as bill, b.created_at as time from bill_participants bp, bills b, users u where bp.bill_id = b.bill_id and b.user_id = u.user_id and bp.user_id = $1;`;
+      const res = await pool.query(query2, [currentUserQuery.rows[0].user_id]);
+
+      if (!res) {
+        return NextResponse.json({ success: false, error: "Bill not found." });
+      }
+      return NextResponse.json({ success: true, bills: res.rows });
+    } catch (error) {
+      return NextResponse.json({ success: false, error: error });
+    }
+  }
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,21 +1,51 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import BillSettle from "../components/ui/bill_settle";
 import withAuth from "../utils/withAuth"; // Adjust the path accordingly
 
 function Dashboard() {
+
+    const [bills, setBills] = useState([]);
+
+    const fetchBills = async () => {
+        const res = await fetch(`/api/bill/pending?user=${localStorage.getItem("username")}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await res.json();
+        if (data?.success) {
+            console.log("bills", data?.bills);
+            setBills(data?.bills);
+        }
+    };
+
+    useEffect(() => {
+        fetchBills();
+    }, []);
+
     return (
         <div className="bg-gray-100 p-4 flex flex-col gap-4 " style={{ height: "90vh" }}>
-            <div className=" bg-white rounded-lg h-80">
+            <div className=" bg-white rounded-lg min-h-72 flex flex-col">
                 <h2 className="text-lg font-semibold mb-4 p-4">Dashboard</h2>
             </div>
-            <div className=" rounded-lg bg-white overflow-scroll">
+            <div className=" rounded-lg bg-white p-2 flex-1">
                 <h2 className="text-lg font-semibold mb-4 p-4">Unsettled Bills</h2>
-                <div className=" grid grid-cols-1 md:grid-cols-3 gap-4 items-center place-items-center p-4">
-                    <BillSettle />
-                    <BillSettle />
-                    <BillSettle />
-                    <BillSettle />
+                <div className="overflow-y-auto max-h-80 p-4">
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center place-items-center ">
+                        {bills.map((bill) => (
+                            <BillSettle
+                                key={bill.id}
+                                id={bill.id}
+                                Name={bill.from}
+                                amount={bill.amount}
+                                time={bill.time}
+                                BillName={bill.bill}
+                            />
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
